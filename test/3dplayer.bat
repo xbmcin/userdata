cbd7903c8d7991b079e98a03946ad90b734baf3b
arcko <arcko@qq.com>
2014-12-01 22:39:22 +0800

    Test scripts for 3dplayer and foobar2000

---
+++ test/3dplayer.bat
@@ -0,0 +1,1 @@
+"C:\Program Files (x86)\DWindow\StereoPlayer.exe" "J:\Videos\002_mkv-AVC_720p_AC3_7.456Mbps.mkv"
---
+++ test/foobar2000.bat
@@ -0,0 +1,2 @@
+"C:\Program Files\foobar2000\foobar2000.exe" "C:\Users\arcko\Music\test.mp3"
+
